c570ade7620c271175eaec05f5205363b623e2a2
alex-beaverg <a.beaverg@gmail.com>
2020-10-06 21:30:36 +0300

    docs: upload task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';   
+    let abc = [];
+    for (let i = 0; i < expr.length / 10; i++) {
+          abc.push(String(Number(expr.slice(i * 10, i * 10 + 10))));
+    }   
+    let a = [];
+    let b = [];
+    for (let i = 0; i < abc.length; i++) {       
+          a[i] = [];
+      b[i] = '';
+      if (!isNaN(abc[i])) {
+                  for (let j = 0; j < abc[i].length / 2; j++) {
+                          a[i].push((abc[i].slice(j * 2, j * 2 + 2)) === '10' ? '.' : '-');
+            b[i] += a[i][j];
+          }
+      } else {
+                  a[i].push(' ');
+          b[i] = ' ';
+        }
+    }   
+    for (let i = 0; i < b.length; i++) {
+          if (b[i] !== ' ') result += MORSE_TABLE[b[i]];
+      else result += b[i];
+    } 
+    return result;
 }
 
 module.exports = {
